f8380306a599049e038723237c64c0c984f5b762
Valentin L <valik2105@mail.ru>
2023-04-19 19:20:16 +0300

    Change comment to Russian in jenkins_server_installation.sh

--- installation/jenkins_server_installation.sh
+++ installation/jenkins_server_installation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Installation of Jenkins
+# Установка Jenkins
 
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
@@ -16,38 +16,38 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
 sudo apt-get update
 sudo apt-get install -y jenkins
 
-# Installation of Java
+# Установка Java
 
 sudo apt install -y openjdk-11-jre
 java -version
 
-# Installation of Git
+# Установка Git
 
 sudo apt install -y git
 
-# Installation of Docker
+# Установка Docker
 
 sudo apt install -y docker.io
 
-# Installation of Cosign
+# Установка Cosign
 
 wget "https://github.com/sigstore/cosign/releases/download/v2.0.0/cosign-linux-amd64"
 mv cosign-linux-amd64 /usr/local/bin/cosign
 chmod +x /usr/local/bin/cosign
 
-# Install grype
+# Установка grype
 
 curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sh -s -- -b /usr/local/bin
 
-# Install jq
+# Установка jq
 
 apt install -y jq
 
-# Add jenkins user to docker group
+# Добавление пользователя jenkins в группу docker
 
 usermod -aG docker jenkins
 
-# Start Jenkins
+# Запуск Jenkins
 
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
